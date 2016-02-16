d32fe735a1fa4f16a26c5e164ea0b48d5e8555d0
Eric Kiara <ekiara@gmail.com>
2016-02-16 14:57:41 +0300

    Add first pdfquery project

--- pdfquery/01__pdfquery_scrape_any_pdf_and_print_dictionary.py
+++ pdfquery/01__pdfquery_scrape_any_pdf_and_print_dictionary.py
@@ -3,4 +3,8 @@
 """
 Scrape any PDF and print the result dictionary.
 
+
+http://schoolofdata.org/2013/08/16/scraping-pdfs-with-python-and-the-scraperwiki-module/
+https://www.reddit.com/r/tifu/comments/450fip/tifu_by_upgrading_my_dads_laptop_to_windows_10/
+
 """
